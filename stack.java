83e96d5f3469a32809378f4de9ad14d987e5ed92
gprajwalm10 <g.prajwal.m.10@gmail.com>
2025-07-06 14:48:18 +0530

    codes

---
+++ stack.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Stack {
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+
+        while(!stack.isEmpty()) {
+            System.out.println(stack.peek());
+            stack.pop();
+        }        
+    }
+}
+
